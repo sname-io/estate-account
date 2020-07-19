abc8a09c2dbd27c31223a9851b6e9dc161f35f57
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-07-19 12:17:40 +0200

    add login page with route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Dairy Farm Account" });
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("index", { title: "Dairy Farm Account" });
 });
 
 module.exports = router;
