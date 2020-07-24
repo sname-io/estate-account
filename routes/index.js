d91d9fff44a5c0a056adb361c21a9a857e5ac180
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-07-24 12:57:04 +0200

    encrypt password

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var { User } = require("../db/models");
 const passport = require("../config/passport/passport");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   res.redirect("/login");
 });
 
