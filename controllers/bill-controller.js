082228cfa2967ad6ddb94951407fac7dd0593298
Olumuyiwa <oluosiname@gmail.com>
2020-07-26 15:17:54 +0200

    list bills

--- controllers/bill-controller.js
+++ controllers/bill-controller.js
@@ -3,7 +3,11 @@ const { Bill } = require("../db/models");
 class BillController {
   static async getAll(req, res, next) {
     const bills = await Bill.findAll();
-    res.render("bills/index", { title: "Bills", active: "bills" });
+    res.render("bills/index", {
+      bills: bills,
+      title: "Bills",
+      active: "bills",
+    });
   }
 }
 
