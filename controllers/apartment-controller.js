a5e1a11b7db5f5a88a3d0e7df14f5368c32b0364
Olumuyiwa <oluosiname@gmail.com>
2020-07-29 09:33:36 +0200

    pagination backend

--- controllers/apartment-controller.js
+++ controllers/apartment-controller.js
@@ -2,12 +2,18 @@ const { Apartment } = require("../db/models");
 
 class ApartmentController {
   static async getAllApartments(req, res, next) {
-    const apartments = await Apartment.findAll();
+    const { page, per_page } = req.query;
+    const limit = per_page || 10;
+    const offset = (page || 0) * limit;
+    const total = await Apartment.count;
+
+    const apartments = await Apartment.findAll({ limit, offset });
 
     res.render("apartments/index", {
       apartments: apartments,
       title: "Apartments",
       active: "apartments",
+      total,
     });
   }
 }
