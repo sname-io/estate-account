287bf0569c09df91e9bad579dc04877ff68f8d03
Olumuyiwa <oluosiname@gmail.com>
2020-08-02 18:25:26 +0200

    fix login

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -6,7 +6,8 @@ passport.use(
   new LocalStrategy(async function (username, password, done) {
     try {
       const user = await User.findOne({ where: { username: username } });
-      if (user && user.isValidPassword(password)) {
+
+      if (user && (await user.isValidPassword(password))) {
         return done(null, user);
       } else {
         return done(null, false, { message: "Incorrect username / password" });
