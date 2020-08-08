7857fdc753ae0947c9b651caae8d38546c245570
Olumuyiwa <oluosiname@gmail.com>
2020-08-08 11:02:46 +0200

    attempt 9

--- tasks/setup-admin.js
+++ tasks/setup-admin.js
@@ -1,2 +1,10 @@
-console.log("I am here");
-console.log("running task");
+const { User } = require("../db/models");
+
+const ADMINS = process.env.ADMINS.split("-");
+
+ADMINS.forEach((a, index) => {
+  console.log(a);
+  console.log(index)[(admin, password)] = a.split(":");
+  console.log("admin ", admin);
+  console.log("password ", password);
+});
