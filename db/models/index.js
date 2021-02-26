0d66315d0a79074124b5003a91f1aa352f6580a7
Oludamilola Osiname <osinameoludamilola@gmail.com>
2021-02-26 19:55:27 +0100

    fix 5

--- db/models/index.js
+++ db/models/index.js
@@ -10,13 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: true,
-      rejectUnauthorized: false,
-    },
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
